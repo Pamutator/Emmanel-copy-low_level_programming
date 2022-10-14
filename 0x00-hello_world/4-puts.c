cf79d7f14706fb9bac7ec9441dc24e7701bb0115
root <akinwmialalade@gmail.com>
2022-10-13 20:13:52 -0700

    101-quote.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main- serves as the entry point for the program
+*
+*Return: By default returns zero if no errors otherwise retrns non-zero value.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
