420b447a0e614bad4bb46de5ccac193bd4454034
root <akinwmialalade@gmail.com>
2022-10-22 14:45:04 -0700

    variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main - print numbers from 00 to 99
+* Return: 0
+*/
+int main(void)
+{
+	int a;
+	int b;
+	a = '0';
+	b = '0';
+	while (a < '9')
+{
+	while (b < '9')
+{
+	if (!(a > b || a == b))
+{
+	if (a == '8' && b == '9')
+{
+		putchar('\n');
+}
+	else
+{
+		putchar(',');
+		putchar(' ');
+}
+}
+		b++;
+		}
+		b = '0';
+		a++;
+}
+	return (0);
+
+}
