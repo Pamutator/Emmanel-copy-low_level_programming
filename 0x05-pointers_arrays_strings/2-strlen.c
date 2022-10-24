dac678787f6a983d3d14183832018f274d0d13c7
root <akinwmialalade@gmail.com>
2022-10-24 15:59:39 -0700

    1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,10 +9,10 @@ int _strlen(char *s)
 {
 	int p = 0;
 
-	/*increment up to the last character is NULL,\0*/ while (*(s + p) != 0)
+	/**
+	 * increment up to the last character is NULL,\0*/ while (*(s + p) != 0)
 	{
 		p++;
 	}
-	
 	return (p);
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@ void puts2(char *str)
 	while (count >= 0)
 	{
 		if (str[count] == '\0')
-	}
-	_putchar('\n');
-	break;
-	{
+		{
+			_putchar('\n');
+			break;
+		}
 		if (count % 2 == 0)
 			_putchar(str[count]);
 		count++;
