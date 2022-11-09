2b12b9f5a93797ec069764217f0e42ffbf52b6dc
root <akinwmialalade@gmail.com>
2022-11-08 20:50:22 -0800

    1-strdup.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -15,25 +15,25 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0)
 		return (NULL);
-	
+
 	for (c = i = 0; i < ac; i++)
 	{
 		if (av[i] == NULL)
 			return (NULL);
-	
+
 		for (j = 0; av[i][j] != '\0'; j++)
 			c++;
 		c++;
 	}
 
 	aout = malloc((c + 1) * sizeof(char));
-	
+
 	if (aout == NULL)
 	{
 		free(aout);
 		return (NULL);
 	}
-	
+
 	for (i = j = ia = 0; ia < c; j++, ia++)
 	{
 		if (av[i][j] == '\0')
@@ -47,6 +47,6 @@ char *argstostr(int ac, char **av)
 			aout[ia] = av[i][j];
 	}
 	aout[ia] = '\0';
-	
+
 	return (aout);
 }
