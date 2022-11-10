fae70a2b840f1858df5f9097aa1dd53c0ba04e2a
root <akinwmialalade@gmail.com>
2022-11-09 20:52:27 -0800

    adjusted

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ * @c: is the character to be checked
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a')
+		return (1);
+	else
+		return (0);
+}
