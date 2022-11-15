9cb8c6d94c11ee08f00ebf135d4e5a4561a42704
root <akinwmialalade@gmail.com>
2022-11-14 20:54:00 -0800

    0x0E-structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -42,7 +42,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
-	
+
 	p_dog->age = age;
 
 	for (i = 0; i < lowner; i++)
