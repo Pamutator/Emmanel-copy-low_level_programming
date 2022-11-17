e1c80c9763aa59d94c8f3630f692d64119f2fb39
root <akinwmialalade@gmail.com>
2022-11-16 21:06:30 -0800

    0-print_name.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -20,26 +20,20 @@ int main(int argc, char *argv[])
 	}
 
 	nbytes = atoi(argv[1]);
+
 	if (nbytes < 0)
+	{
 		printf("Error\n");
 	exit(2);
-}
-for (i = 0; i < nbytes; i++)
+	}
 
-									{
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
 
-												printf("%02x", opc[i] & 0xFF);
-
-														if (i != nbytes - 1)
-
-																		printf(" ");
-
-															}
-
-
-
-								printf("\n");
-
-									return (0);
-
+	printf("\n");
+	return (0);
 }
