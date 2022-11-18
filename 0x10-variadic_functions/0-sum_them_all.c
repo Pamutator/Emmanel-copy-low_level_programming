8187661bb64a3d5e443a444593ff04b4ac8b0c27
root <akinwmialalade@gmail.com>
2022-11-17 20:08:54 -0800

    0x10-variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	
+
 	va_start(valist, n);
-	
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
-	
+
 	va_end(valist);
-	
+
 	return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,18 +12,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 	char *str;
-	
+
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(valist, char *);
-		
+
 		if (str)
 			printf("%s", str);
 		else
 			printf("(nil)");
-		
+
 		if (i < n - 1)
 			if (separator)
 				printf("%s", separator);
